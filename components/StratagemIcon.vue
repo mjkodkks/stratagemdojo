b5204d8099179a058f678615875ac0351aaa95df
Thanonphat.su (DKKs) <thanonphat.supho@krungsri.com>
2024-03-25 12:38:17 +0700

    [Add] remove unused code

---
+++ components/StratagemIcon.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+interface Props {
+
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div />
+</template>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <main class="py-20 px-10 text-center">
     <slot />
-    <Footer />
     <div class="mt-5 mx-auto text-center opacity-25 text-sm text-[20px]">
       [Default Layout]
     </div>
